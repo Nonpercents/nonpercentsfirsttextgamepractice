dab271c695de143f2360c95ad0a66818c44ac4c7
Nonpercents <32957431+Nonpercents@users.noreply.github.com>
2021-02-23 11:38:38 +0900

    Improving game

--- gamescript.js
+++ gamescript.js
@@ -6,23 +6,37 @@ function pleasewaitscene(golevel) {
 	setTimeout(function(){gamestatus(golevel)}, 500);
 }
 
-function gamestatus(gamelevel) {
+function gameoutofrange() {
+	document.getElementById('gametext').innerHTML = "OUT OF RANGE";
+	document.getElementById('chooselevels').innerHTML = "ERROR";
+}
+
+function gamestatus(gamelevel) { 
 	var gamelisttext="";
-	document.getElementById('gametext').innerHTML = gamelist[gamelevel][0];
-	for (i = 0; i<gamelist[gamelevel][1].length; i++){
-		gamelisttext += '<li><a style="cursor: pointer;" onclick="pleasewaitscene('+gamelist[gamelevel][2][i]+');">'+gamelist[gamelevel][1][i]+'<a/></li>';
+	if (gamelevel>=gamelist.length) {
+		gameoutofrange();
+	} else {
+		document.getElementById('gametext').innerHTML = gamelist[gamelevel][0];
+		if (gamelist[gamelevel][1].length !== 0) {
+			for (i = 0; i<gamelist[gamelevel][1].length; i++){
+				gamelisttext += '<li><a onclick="pleasewaitscene('+gamelist[gamelevel][2][i]+');">'+gamelist[gamelevel][1][i]+'<a/></li>';
+			}
+			gamelisttext += '<br><small><i>choice loading ended.</i></small>';
+			document.getElementById('chooselevels').innerHTML = gamelisttext;
+		} else {
+			document.getElementById('chooselevels').innerHTML = '<small><i>this level of the game doesn&#39;t have any choice.<br><div align="center">(game over)</div></i></small>';
+		}
 	}
-	gamelisttext += '<br><small><i>choice loading ended.</i></small>';
-	document.getElementById('chooselevels').innerHTML = gamelisttext;
 }
 
 function maingame () {
-	document.getElementById('credit').innerHTML = "TEXT SELECT ADVENTURE GAME GAMES 2021–"+(new Date().getFullYear());
+	document.getElementById('credit').innerHTML = "TEXT SELECT ADVENTURE GAME GAMES 1996–"+(new Date().getFullYear());
 	for (i = 0; i<gamelist.length; i++) {
-		if (gamelist[i].length == 3 && gamelist[i][1].length == gamelist[i][2].length) {
+		if (typeof gamelist[i][0] == "string" && gamelist[i].length == 3 && gamelist[i][1].length == gamelist[i][2].length) {
 			continue;
 		} else {
-			chooseinequal = true
+			chooseinequal = true;
+			break;
 		}
 	}
 	if (chooseinequal == true) {
